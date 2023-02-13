a5df9851411005e065b8163261fdb9183a28375e
lixiaopan <lixiaopan@jd.com>
2023-02-13 15:35:44 +0800

    fix: 修改对外暴露模块

--- packages/form/src/main.ts
+++ packages/form/src/main.ts
@@ -1,6 +1,7 @@
 // import { readonly } from 'vue'
 import SchemaForm from './Index.vue'
 import useForm from './useForm.js'
+import SchemaFormItem from './SchemaFormItem.vue'
 
 SchemaForm.install = (app, opt = {}): void => {
     app.component(SchemaForm?.name, SchemaForm)
@@ -9,5 +10,6 @@ SchemaForm.install = (app, opt = {}): void => {
 // export default SchemaForm
 export default {
     SchemaForm,
-    useForm
+    useForm,
+    SchemaFormItem
 };
